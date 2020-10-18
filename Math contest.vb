06f218264248127f2052764a972a6646882026fc
Ivan <the_real_iori_orochi@hotmail.com>
2020-10-18 16:38:29 -0600

    Added code

---
+++ Math contest.vb
@@ -0,0 +1,23 @@
+﻿
+
+
+Public Class Form1
+    Dim num1 As Integer
+    Dim num2 As Integer
+    Dim answer As Integer
+    Dim questions As Integer = 0I
+    Dim correctAnswer As Integer = 0I
+
+    Private Sub exitButton_Click(sender As Object, e As EventArgs) Handles exitButton.Click
+
+        Me.Close()
+
+    End Sub
+
+    Private Sub resultButton_Click(sender As Object, e As EventArgs) Handles resultButton.Click
+
+        MsgBox(correctAnswer & "/" & questions & "Correct answers")
+    End Sub
+
+
+End Class
