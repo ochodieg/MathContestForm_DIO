affbb906ba7e0fce1137b17e77d500a7155e4631
Ivan <the_real_iori_orochi@hotmail.com>
2020-10-18 15:35:57 -0600

    accidentally deleted everything and had to remake

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -24,14 +24,22 @@ Partial Class Form1
     Private Sub InitializeComponent()
         Me.nameTextBox = New System.Windows.Forms.TextBox()
         Me.nameLabel = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.GroupBox3 = New System.Windows.Forms.GroupBox()
+        Me.ageLabel = New System.Windows.Forms.Label()
+        Me.gradeLabel = New System.Windows.Forms.Label()
+        Me.GroupBox1.SuspendLayout()
         Me.SuspendLayout()
         '
         'nameTextBox
         '
-        Me.nameTextBox.Location = New System.Drawing.Point(53, 65)
+        Me.nameTextBox.Location = New System.Drawing.Point(6, 73)
         Me.nameTextBox.Multiline = True
         Me.nameTextBox.Name = "nameTextBox"
-        Me.nameTextBox.Size = New System.Drawing.Size(154, 26)
+        Me.nameTextBox.Size = New System.Drawing.Size(304, 42)
         Me.nameTextBox.TabIndex = 0
         '
         'nameLabel
@@ -39,26 +47,106 @@ Partial Class Form1
         Me.nameLabel.AutoSize = True
         Me.nameLabel.BackColor = System.Drawing.SystemColors.ActiveCaptionText
         Me.nameLabel.ForeColor = System.Drawing.SystemColors.ButtonHighlight
-        Me.nameLabel.Location = New System.Drawing.Point(49, 25)
+        Me.nameLabel.Location = New System.Drawing.Point(6, 41)
         Me.nameLabel.Name = "nameLabel"
         Me.nameLabel.Size = New System.Drawing.Size(51, 20)
         Me.nameLabel.TabIndex = 1
         Me.nameLabel.Text = "Name"
         '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(316, 73)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(91, 42)
+        Me.TextBox1.TabIndex = 2
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(413, 73)
+        Me.TextBox2.Multiline = True
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(91, 42)
+        Me.TextBox2.TabIndex = 3
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.gradeLabel)
+        Me.GroupBox1.Controls.Add(Me.ageLabel)
+        Me.GroupBox1.Controls.Add(Me.TextBox2)
+        Me.GroupBox1.Controls.Add(Me.nameLabel)
+        Me.GroupBox1.Controls.Add(Me.TextBox1)
+        Me.GroupBox1.Controls.Add(Me.nameTextBox)
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 12)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(542, 169)
+        Me.GroupBox1.TabIndex = 4
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "GroupBox1"
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Location = New System.Drawing.Point(18, 205)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(212, 242)
+        Me.GroupBox2.TabIndex = 5
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "GroupBox2"
+        '
+        'GroupBox3
+        '
+        Me.GroupBox3.Location = New System.Drawing.Point(310, 213)
+        Me.GroupBox3.Name = "GroupBox3"
+        Me.GroupBox3.Size = New System.Drawing.Size(244, 234)
+        Me.GroupBox3.TabIndex = 6
+        Me.GroupBox3.TabStop = False
+        Me.GroupBox3.Text = "GroupBox3"
+        '
+        'ageLabel
+        '
+        Me.ageLabel.AutoSize = True
+        Me.ageLabel.BackColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.ageLabel.ForeColor = System.Drawing.SystemColors.ButtonFace
+        Me.ageLabel.Location = New System.Drawing.Point(312, 41)
+        Me.ageLabel.Name = "ageLabel"
+        Me.ageLabel.Size = New System.Drawing.Size(38, 20)
+        Me.ageLabel.TabIndex = 4
+        Me.ageLabel.Text = "Age"
+        '
+        'gradeLabel
+        '
+        Me.gradeLabel.AutoSize = True
+        Me.gradeLabel.BackColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.gradeLabel.ForeColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.gradeLabel.Location = New System.Drawing.Point(409, 41)
+        Me.gradeLabel.Name = "gradeLabel"
+        Me.gradeLabel.Size = New System.Drawing.Size(54, 20)
+        Me.gradeLabel.TabIndex = 5
+        Me.gradeLabel.Text = "Grade"
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Controls.Add(Me.nameLabel)
-        Me.Controls.Add(Me.nameTextBox)
+        Me.ClientSize = New System.Drawing.Size(898, 472)
+        Me.Controls.Add(Me.GroupBox3)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
         Me.Name = "Form1"
         Me.Text = "Form1"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
         Me.ResumeLayout(False)
-        Me.PerformLayout()
 
     End Sub
 
     Friend WithEvents nameTextBox As TextBox
     Friend WithEvents nameLabel As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents gradeLabel As Label
+    Friend WithEvents ageLabel As Label
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents GroupBox3 As GroupBox
 End Class
