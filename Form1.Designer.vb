47d1e53fea47527e135e89eba77d239095323e64
Ivan <the_real_iori_orochi@hotmail.com>
2020-10-18 15:45:25 -0600

    Still building

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -24,14 +24,19 @@ Partial Class Form1
     Private Sub InitializeComponent()
         Me.nameTextBox = New System.Windows.Forms.TextBox()
         Me.nameLabel = New System.Windows.Forms.Label()
-        Me.TextBox1 = New System.Windows.Forms.TextBox()
-        Me.TextBox2 = New System.Windows.Forms.TextBox()
-        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
-        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.ageTextBox = New System.Windows.Forms.TextBox()
+        Me.gradeTextBox = New System.Windows.Forms.TextBox()
+        Me.studentinfoBox = New System.Windows.Forms.GroupBox()
+        Me.functionGroupBox = New System.Windows.Forms.GroupBox()
         Me.GroupBox3 = New System.Windows.Forms.GroupBox()
         Me.ageLabel = New System.Windows.Forms.Label()
         Me.gradeLabel = New System.Windows.Forms.Label()
-        Me.GroupBox1.SuspendLayout()
+        Me.addButton = New System.Windows.Forms.RadioButton()
+        Me.subtractButton = New System.Windows.Forms.RadioButton()
+        Me.multiplyButton = New System.Windows.Forms.RadioButton()
+        Me.divideButton = New System.Windows.Forms.RadioButton()
+        Me.studentinfoBox.SuspendLayout()
+        Me.functionGroupBox.SuspendLayout()
         Me.SuspendLayout()
         '
         'nameTextBox
@@ -53,51 +58,55 @@ Partial Class Form1
         Me.nameLabel.TabIndex = 1
         Me.nameLabel.Text = "Name"
         '
-        'TextBox1
+        'ageTextBox
         '
-        Me.TextBox1.Location = New System.Drawing.Point(316, 73)
-        Me.TextBox1.Multiline = True
-        Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(91, 42)
-        Me.TextBox1.TabIndex = 2
+        Me.ageTextBox.Location = New System.Drawing.Point(316, 73)
+        Me.ageTextBox.Multiline = True
+        Me.ageTextBox.Name = "ageTextBox"
+        Me.ageTextBox.Size = New System.Drawing.Size(91, 42)
+        Me.ageTextBox.TabIndex = 2
         '
-        'TextBox2
+        'gradeTextBox
         '
-        Me.TextBox2.Location = New System.Drawing.Point(413, 73)
-        Me.TextBox2.Multiline = True
-        Me.TextBox2.Name = "TextBox2"
-        Me.TextBox2.Size = New System.Drawing.Size(91, 42)
-        Me.TextBox2.TabIndex = 3
+        Me.gradeTextBox.Location = New System.Drawing.Point(413, 73)
+        Me.gradeTextBox.Multiline = True
+        Me.gradeTextBox.Name = "gradeTextBox"
+        Me.gradeTextBox.Size = New System.Drawing.Size(91, 42)
+        Me.gradeTextBox.TabIndex = 3
         '
-        'GroupBox1
+        'studentinfoBox
         '
-        Me.GroupBox1.Controls.Add(Me.gradeLabel)
-        Me.GroupBox1.Controls.Add(Me.ageLabel)
-        Me.GroupBox1.Controls.Add(Me.TextBox2)
-        Me.GroupBox1.Controls.Add(Me.nameLabel)
-        Me.GroupBox1.Controls.Add(Me.TextBox1)
-        Me.GroupBox1.Controls.Add(Me.nameTextBox)
-        Me.GroupBox1.Location = New System.Drawing.Point(12, 12)
-        Me.GroupBox1.Name = "GroupBox1"
-        Me.GroupBox1.Size = New System.Drawing.Size(542, 169)
-        Me.GroupBox1.TabIndex = 4
-        Me.GroupBox1.TabStop = False
-        Me.GroupBox1.Text = "GroupBox1"
+        Me.studentinfoBox.Controls.Add(Me.gradeLabel)
+        Me.studentinfoBox.Controls.Add(Me.ageLabel)
+        Me.studentinfoBox.Controls.Add(Me.gradeTextBox)
+        Me.studentinfoBox.Controls.Add(Me.nameLabel)
+        Me.studentinfoBox.Controls.Add(Me.ageTextBox)
+        Me.studentinfoBox.Controls.Add(Me.nameTextBox)
+        Me.studentinfoBox.Location = New System.Drawing.Point(12, 12)
+        Me.studentinfoBox.Name = "studentinfoBox"
+        Me.studentinfoBox.Size = New System.Drawing.Size(542, 169)
+        Me.studentinfoBox.TabIndex = 4
+        Me.studentinfoBox.TabStop = False
+        Me.studentinfoBox.Text = "Student Info."
         '
-        'GroupBox2
+        'functionGroupBox
         '
-        Me.GroupBox2.Location = New System.Drawing.Point(18, 205)
-        Me.GroupBox2.Name = "GroupBox2"
-        Me.GroupBox2.Size = New System.Drawing.Size(212, 242)
-        Me.GroupBox2.TabIndex = 5
-        Me.GroupBox2.TabStop = False
-        Me.GroupBox2.Text = "GroupBox2"
+        Me.functionGroupBox.Controls.Add(Me.divideButton)
+        Me.functionGroupBox.Controls.Add(Me.multiplyButton)
+        Me.functionGroupBox.Controls.Add(Me.subtractButton)
+        Me.functionGroupBox.Controls.Add(Me.addButton)
+        Me.functionGroupBox.Location = New System.Drawing.Point(18, 205)
+        Me.functionGroupBox.Name = "functionGroupBox"
+        Me.functionGroupBox.Size = New System.Drawing.Size(212, 242)
+        Me.functionGroupBox.TabIndex = 5
+        Me.functionGroupBox.TabStop = False
+        Me.functionGroupBox.Text = "Function Select"
         '
         'GroupBox3
         '
-        Me.GroupBox3.Location = New System.Drawing.Point(310, 213)
+        Me.GroupBox3.Location = New System.Drawing.Point(262, 205)
         Me.GroupBox3.Name = "GroupBox3"
-        Me.GroupBox3.Size = New System.Drawing.Size(244, 234)
+        Me.GroupBox3.Size = New System.Drawing.Size(292, 242)
         Me.GroupBox3.TabIndex = 6
         Me.GroupBox3.TabStop = False
         Me.GroupBox3.Text = "GroupBox3"
@@ -124,29 +133,79 @@ Partial Class Form1
         Me.gradeLabel.TabIndex = 5
         Me.gradeLabel.Text = "Grade"
         '
+        'addButton
+        '
+        Me.addButton.AutoSize = True
+        Me.addButton.Location = New System.Drawing.Point(48, 41)
+        Me.addButton.Name = "addButton"
+        Me.addButton.Size = New System.Drawing.Size(63, 24)
+        Me.addButton.TabIndex = 0
+        Me.addButton.TabStop = True
+        Me.addButton.Text = "Add"
+        Me.addButton.UseVisualStyleBackColor = True
+        '
+        'subtractButton
+        '
+        Me.subtractButton.AutoSize = True
+        Me.subtractButton.Location = New System.Drawing.Point(48, 91)
+        Me.subtractButton.Name = "subtractButton"
+        Me.subtractButton.Size = New System.Drawing.Size(95, 24)
+        Me.subtractButton.TabIndex = 1
+        Me.subtractButton.TabStop = True
+        Me.subtractButton.Text = "Subtract"
+        Me.subtractButton.UseVisualStyleBackColor = True
+        '
+        'multiplyButton
+        '
+        Me.multiplyButton.AutoSize = True
+        Me.multiplyButton.Location = New System.Drawing.Point(48, 146)
+        Me.multiplyButton.Name = "multiplyButton"
+        Me.multiplyButton.Size = New System.Drawing.Size(86, 24)
+        Me.multiplyButton.TabIndex = 2
+        Me.multiplyButton.TabStop = True
+        Me.multiplyButton.Text = "Multiply"
+        Me.multiplyButton.UseVisualStyleBackColor = True
+        '
+        'divideButton
+        '
+        Me.divideButton.AutoSize = True
+        Me.divideButton.Location = New System.Drawing.Point(48, 199)
+        Me.divideButton.Name = "divideButton"
+        Me.divideButton.Size = New System.Drawing.Size(77, 24)
+        Me.divideButton.TabIndex = 3
+        Me.divideButton.TabStop = True
+        Me.divideButton.Text = "Divide"
+        Me.divideButton.UseVisualStyleBackColor = True
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(898, 472)
         Me.Controls.Add(Me.GroupBox3)
-        Me.Controls.Add(Me.GroupBox2)
-        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.functionGroupBox)
+        Me.Controls.Add(Me.studentinfoBox)
         Me.Name = "Form1"
         Me.Text = "Form1"
-        Me.GroupBox1.ResumeLayout(False)
-        Me.GroupBox1.PerformLayout()
+        Me.studentinfoBox.ResumeLayout(False)
+        Me.studentinfoBox.PerformLayout()
+        Me.functionGroupBox.ResumeLayout(False)
+        Me.functionGroupBox.PerformLayout()
         Me.ResumeLayout(False)
 
     End Sub
 
     Friend WithEvents nameTextBox As TextBox
     Friend WithEvents nameLabel As Label
-    Friend WithEvents TextBox1 As TextBox
-    Friend WithEvents TextBox2 As TextBox
-    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents ageTextBox As TextBox
+    Friend WithEvents gradeTextBox As TextBox
+    Friend WithEvents studentinfoBox As GroupBox
     Friend WithEvents gradeLabel As Label
     Friend WithEvents ageLabel As Label
-    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents functionGroupBox As GroupBox
     Friend WithEvents GroupBox3 As GroupBox
+    Friend WithEvents divideButton As RadioButton
+    Friend WithEvents multiplyButton As RadioButton
+    Friend WithEvents subtractButton As RadioButton
+    Friend WithEvents addButton As RadioButton
 End Class
